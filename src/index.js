4dcf983a9fc37d0d08a916b30d06c51d7de72525
LocS3R <locdev002@gmail.com>
2023-09-02 22:18:45 +0700

    Finish init Project

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import morgan from "morgan";
+import { engine } from "express-handlebars";
+
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = 3000;
+// http logger
+app.use(morgan("combined"));
+
+// templates engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
